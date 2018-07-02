b8ae85faba84de98fff0245c553508670c52198e
Nathan T Case <unclenate62@gmail.com>
2018-07-02 22:54:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatlesArray = [];
   for (var i = 0; i < musicians.length; i++) {
-    beatlesArray.push(musicans[i] + " plays " + instruments[i])
+    beatlesArray.push(musicians[i] + " plays " + instruments[i])
   }
   return beatlesArray
 }
