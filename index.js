b3ca6a3e5550d4d630f7f9227fde1443819505dd
Nathan T Case <unclenate62@gmail.com>
2018-07-02 22:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var beatlesArray = [];
+  var beatlesArray = new Array();
   for (var i = 0; i < musicians.length; i++) {
     beatlesArray.push(musicans[i} + " plays " + instruments[i]])
   }
